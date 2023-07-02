bc037f5f2a4226b59c5970afe1c787080e36242a
Ankur Kumar <akit9815@gmail.com>
2023-07-02 19:16:51 +0530

    Added sidebar and main section with basic styles

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,32 @@ function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className='app'>
+      <section className = 'side-bar'>
+          <button>+ New Chat</button>
+          <ul className='history'>
+              <li>Hello</li>
+          </ul>
+          <nav>
+              <p>DEMO</p>
+          </nav>
+      </section>
+      <section className='main'>
+          <h1>ChatGPT Clone</h1>
+          <ul className='feed'>
+
+          </ul>
+          <div className='bottom-section'>
+              <div className="input-container">
+                  <input type="text" />
+                  <div id="submit">➢</div>
+              </div>
+              <p className='info'>
+                  free preview
+              </p>
+          </div>
+      </section>
+    </div>
   )
 }
 
