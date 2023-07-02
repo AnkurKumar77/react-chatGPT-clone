c2f74e5dafe68d701a39323018ee524563ecfe4b
Ankur Kumar <akit9815@gmail.com>
2023-07-03 01:19:07 +0530

    front-end to back-end request and response handling

--- server.js
+++ server.js
@@ -8,4 +8,32 @@ app.use(express.json()) // allow to work with json when we send
                         // request from front-end to back-end
 app.use(cors())
 
+const API_KEY = ""
+app.post('/completions',async (req,res)=> {
+    
+    const options = {
+        method: "POST",
+        headers: {
+            Authorization : `Bearer ${API_KEY}`,
+            "Content-Type":'application/json'
+        },
+        body: JSON.stringify({
+            model: 'gpt-3.5-turbo',
+            messages: [{
+                role:'user',
+                content:req.body.message
+            }],
+            max_tokens: 100
+        })
+    }
+    try {
+
+        const response = await fetch('https://api.openai.com/v1/chat/completions',options)
+        const data = await response.json()
+        res.send(data)
+    }catch (err) {
+        console.error(err)
+    }
+})
+
 app.listen(PORT,()=> console.log('Your server is running on PORT '+PORT))
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
-import { useState } from 'react'
+import { useState , useEffect } from 'react'
 
 import './App.css'
 
 function App() {
   // const [count, setCount] = useState(0)
+  const [value,setValue] = useState('')
+  const [message, setMessage] = useState(null)
 
+  const getMessages = async ()=> {
+    // console.log('he')
+    const options = {
+      method:'POST',
+      headers: {
+        'Content-Type':'application/json'
+      },
+      body: JSON.stringify({
+        message: value
+      })
+    }
+
+    try {
+      console.log(value)
+
+      const response = await fetch('http://localhost:8000/completions',options)
+      const data = await response.json()
+      setMessage(data.choices[0].message)
+
+    }catch (err) {
+      console.error(err)
+    }
+  }
+  console.log(message)
   return (
     <div className='app'>
       <section className = 'side-bar'>
@@ -23,8 +49,8 @@ function App() {
           </ul>
           <div className='bottom-section'>
               <div className="input-container">
-                  <input type="text" />
-                  <div id="submit">➢</div>
+                  <input type="text" value = {value} onChange={(e)=>setValue(e.target.value)}/>
+                  <div id="submit" onClick={getMessages}>➢</div>
               </div>
               <p className='info'>
                   free preview
